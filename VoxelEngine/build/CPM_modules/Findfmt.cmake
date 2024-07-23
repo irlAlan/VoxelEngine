cf4e3b9f5b0be78deb09f39d08faacd3535d85b3
irlAlan <momoriallan@gmail.com>
2024-07-23 18:18:57 +0100

    changing from cpm to fetchcontent

--- VoxelEngine/build/CPM_modules/Findfmt.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/irlalan/secret/Cpp/VoxelEngine/VoxelEngine/build/cmake/CPM_0.40.0.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;fmtlib/fmt;GIT_TAG;7.1.3;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(fmt_FOUND TRUE)
