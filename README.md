4e19f5e2de771b88d3e5ed650605eb2315391a5f
irlalan <momoriallan@gmail.com>
2025-04-21 04:17:56 +0100

    changes

--- README.md
+++ README.md
@@ -3,37 +3,43 @@
 Attempt to make a voxel engine using opengl
 
 ## Goals of engine
-    - To be able to create and manage windows on linux, windows 
-        - platform layer (abstracts from windows and linux)
-        - Windowing, input, console interaction
-    - Logging
-    - File I/O
-    - Application Layer(handles high level stuff like game loop and calling renderer etc)
-    - Renderer/API abstraction layer (voxels) maybe raymarching
-    - ECS
-    - Physics engine
-    - To be able to optimise and create voxels
-    - To add realistic -ish physics to the voxels
-    - Package in a dll for others to hopefully use
-    - To learn more about program design and architecture as well as data structures
+    - To have a fully functional game engine thats able to render voxels with relative ease to the screen
+    - What I want to be added:
+        - To be able to create and manage windows on linux, windows 
+            - platform layer (abstracts from windows and linux)
+            - Windowing, input, console interaction
+        - Logging
+        - File I/O
+        - Application Layer(handles high level stuff like game loop and calling renderer etc)
+        - Renderer/API abstraction layer (voxels) maybe raymarching
+        - ECS
+        - Physics engine
+        - To be able to optimise and create voxels
+        - To add realistic -ish physics to the voxels
+        - Package in a dll for others to hopefully use
+        - To learn more about program design and architecture as well as data structures
 
 
 ### Layers
 
     Platform Layer:
-        - Windows, File I/O, Console I/O, Renderer API extensions, File I/O
+        - Windows, File I/O, Console I/O, Renderer API extensions
 
     Core:
-        - Logging, Assertions, Profiling, Data structures, async file i/o, engine config, memory allocation, math lib
+        - Logging, Assertions, Profiling, Data structures, async file i/o, engine config, math lib
 
     resource management:
         - Meshes, Materials, Images, Animations
 etc
 
 ## Currently at
+    - File I/O wrapper
     - architecture of the engine
         - Currently making abstract classes for the interface that inherit behaviour from concrete classes through UML diagrams
         - Researching Program architecture
+
+## Finished
+    - Logging and assertions
 
 ## TODO:
     - [x] Add easier type defs and how to do logging and asserts in c++
--- VETest/src/main.cpp
+++ VETest/src/main.cpp
@@ -1,30 +1,89 @@
-#include <VEngine/Core/Asserts.h>
-#include <VEngine/VEngine.h>
+// #include <GLFW/glfw3.h>
+// #include <VEngine/Core/Asserts.h>
 #include <VEngine/Core/Logger.h>
-#include <fmt/core.h>
 #include <VEngine/Core/Window.h>
+#include <VEngine/VEngine.h>
+#include <fmt/core.h>
+#include <glad/glad.h>
 
-class Engine : public VEngine::VEngine
-{
-public:
+#include "VEngine/Core/CoreIncludes.h"
 
-  Engine(){}
-  ~Engine(){
-    fmt::println("ENGINE destruct called");
-  }
-  void onInit() override{fmt::println("On initialise");}
-  void onQuit() override {fmt::println("Quiting");}
-  void run(){fmt::println("RUNNINGJK");}
-  void quit(){}
+class Engine : public VEngine::VEngine {
+ public:
+  Engine() {}
+  ~Engine() { fmt::println("ENGINE destruct called"); }
+  void onInit() override { fmt::println("On initialise"); }
+  void onQuit() override { fmt::println("Quiting"); }
+  void run() { fmt::println("RUNNING"); }
+  void quit() {}
 };
 
-int main()
-{
+int main() {
   // Log::Instance().VTRACE("not good {}", "other");
 
-  VEngine::WindowConf winConf{"Hey there",{600, 500},{1,1,1,1},false};
+  VEngine::WindowConf winConf{"Hey there", {600, 500}, {1, 1, 1, 1}, false};
   VEngine::Window win{winConf};
-  while(true){
+  glEnable(GL_DEPTH_TEST);
+  constexpr float cubeVerts[] = {
+      0.0f, 0.0f,
+      0.0f,  // 111 -- 0
+      1.0f, 0.0f,
+      0.0f,  // 011 -- 1
+      1.0f, 0.0f,
+      1.0f,  // 010 -- 2
+      0.0f, 0.0f,
+      1.0f,  // 110 -- 3
+
+      1.0f, 1.0f,
+      0.0f,  // 001 -- 4
+      1.0f, 1.0f,
+      1.0f,  // 000 -- 5
+      0.0f, 1.0f,
+      1.0f,  // 100 -- 6
+      0.0f, 1.0f,
+      0.0f,  // 101 -- 7
+  };
+
+  constexpr uint indicies[] = {// Top
+                               0, 1, 2, 2, 3, 0,
+
+                               // front
+                               3, 2, 5, 5, 6, 3,
+
+                               // right
+                               7, 0, 3, 3, 6, 7,
+
+                               // back
+                               4, 1, 0, 0, 7, 4,
+
+                               // left
+                               5, 2, 1, 1, 4, 5,
+
+                               // bottom
+                               4, 7, 6, 6, 5, 4};
+  uint VBO, VAO, EBO;
+
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies,
+               GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), &cubeVerts, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+  uint voxelBuffer;
+  glGenBuffers(1, &voxelBuffer);
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, voxelBuffer);
+
+  while (true) {
     win.displayWindow();
   }
   return 0;
